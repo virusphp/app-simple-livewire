619e3c0bbfb374984336d0156cdbb11b27f9ae80
virusphp <packercyber@gmail.com>
2022-12-30 21:37:22 +0700

    create gudang livewire component

---
+++ app/Http/Livewire/Master/Gudang/GudangIndex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Master\Gudang;
+
+use Livewire\Component;
+
+class GudangIndex extends Component
+{
+    public function render()
+    {
+        return view('livewire.master.gudang.gudang-index');
+    }
+}
