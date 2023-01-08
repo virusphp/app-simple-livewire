9e52cbb62403202f0451aa92a282027aec1795e1
virusphp <packercyber@gmail.com>
2023-01-08 19:48:32 +0700

    change logo and name project

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-jet-application-mark class="block h-9 w-auto" />
+                        <img class="block h-9 w-auto" src="{{ asset('img/profile/logo.png') }}" alt="logo" />
                     </a>
                 </div>
 
