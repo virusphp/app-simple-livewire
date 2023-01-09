c7950be4d3a95f2e12207769524de6896d2a1b73
virusphp <packercyber@gmail.com>
2023-01-10 06:01:51 +0700

    add componenet slider

---
+++ app/Http/Livewire/Maseter/Slider/SliderIndex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Maseter\Slider;
+
+use Livewire\Component;
+
+class SliderIndex extends Component
+{
+    public function render()
+    {
+        return view('livewire.maseter.slider.slider-index');
+    }
+}
